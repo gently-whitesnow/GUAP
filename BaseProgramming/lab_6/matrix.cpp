3c4c9663b61d469322ed58580b4fed081fc585a2
Александр <34779498+Canek4005@users.noreply.github.com>
2023-01-06 20:56:28 +0300

    end lab_6

--- BaseProgramming/lab_6/matrix.cpp
+++ BaseProgramming/lab_6/matrix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
 
 using namespace std;
 
@@ -10,20 +11,23 @@ using namespace std;
 // Найти сумму элементов в тех столбцах, которые содержат хотя бы один отрицательный
 // элемент.
 
-bool isIntNumber(string s)
+static const int INVALID_VALUE = std::numeric_limits<int>::min();
+
+bool isIntNumber(const string &s)
 {
+    int start = 0;
     if (!s.empty() && s[0] == '-')
-        s.erase(s.begin());
-    bool b = true;
-    for (int i = 0; i < s.size(); i++)
+        ++start;
+
+    for (int i = start; i < s.size(); i++)
     {
         if (!isdigit(s[i]))
-            b = false;
+            return false;
     }
-    return b;
+    return true;
 }
 
-int getIntValue(string s, bool isNotUint = true)
+int getIntValue(const string &s, bool isNotUint = true)
 {
     string a;
     int b;
@@ -44,53 +48,136 @@ int getIntValue(string s, bool isNotUint = true)
     return b;
 }
 
+void print(int **matrix, int h, int w)
+{
+    cout << "Your matrix:" << endl;
+    for (size_t i = 0; i < h; i++)
+    {
+        for (size_t j = 0; j < w; j++)
+        {
+            if (matrix[i][j] != INVALID_VALUE)
+                cout << matrix[i][j] << "\t";
+            else
+                cout << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void evaluateCharacters(int **matrix, int h, int w)
+{
+    for (size_t i = 0; i < h; i++)
+    {
+        for (size_t j = 0; j < w; j++)
+        {
+            int val = matrix[i][j];
+            if (val % 2 != 0 && val < 0)
+            {
+                matrix[h][j] += abs(val);
+            }
+        }
+    }
+}
+int partition(int **matrix, int h, int first, int last)
+{
+    int average = matrix[h][(first + last) / 2];
+    int i = first;
+    int j = last;
+    while (i <= j)
+    {
+        while (matrix[h][i] < average)
+            i++;
+        while (matrix[h][j] > average)
+            j--;
+        if (i >= j)
+            break;
+        for (int k = 0; k <= h; k++)
+        {
+            if (matrix[h][i] != matrix[h][j])
+                swap(matrix[k][j], matrix[k][i]);
+        }
+        i++;
+        j--;
+    }
+    return j;
+}
+
+void quickSort(int **matrix, int h, int first, int last)
+{
+    if (first < last)
+    {
+        int half = partition(matrix, h, first, last);
+        quickSort(matrix, h, first, half);
+        quickSort(matrix, h, half + 1, last);
+    }
+}
+
+void sumIfNegative(int **matrix, int h, int w)
+{
+    for (size_t i = 0; i < w; i++)
+    {
+        bool haveNegative = false;
+        // зануляем коэффициенты после предыдущей задачи
+        matrix[h][i] = 0; 
+        for (size_t j = 0; j < h; j++)
+        {
+            int val = matrix[j][i];
+            matrix[h][i] += val;
+            if (val < 0)
+                haveNegative = true;
+        }
+        if (!haveNegative)
+            matrix[h][i] = INVALID_VALUE;
+    }
+}
+
 int main()
 {
+
     // запросить размер матрицы w и h
     unsigned int w = getIntValue("matrix width", false);
     unsigned int h = getIntValue("matrix height", false);
 
     // Создаем массив указателей - количество строк
     // Также выделим +1 памяти под задачи варианта
-    int** matrix = new int*[h+1];
+    int **matrix = new int *[h + 1];
     // запросить значения матрицы
-    for (size_t i = 0; i < h; i++)
+    for (size_t i = 0; i < h + 1; i++)
     {
-        // для каждого указателя на строку 
+        // для каждого указателя на строку
         // выделяем память под массив значений столбца
         matrix[i] = new int[w];
         for (size_t j = 0; j < w; j++)
         {
-            matrix[i][j] = getIntValue("column " + to_string(i) + " row " + to_string(j));
+            if (i != h)
+                matrix[i][j] = getIntValue("column " + to_string(j) + " row " + to_string(i));
+            else
+                matrix[i][j] = 0;
         }
     }
+
+    // Исходная матрица
+    print(matrix, h + 1, w);
 
     // для каждого столбца посчитать характеристику
+    evaluateCharacters(matrix, h, w);
+    print(matrix, h + 1, w);
 
     // сортируем
+    quickSort(matrix, h, 0, w - 1);
+    print(matrix, h + 1, w);
 
-    // печатаем (если не null)
-
-    // считаем 2 задачу (остальные столбцы проставляем в null)
-
-    // печатаем (если не null)
-    
-    cout << "Your matrix:" << endl;
-    for (size_t i = 0; i < h; i++)
-    {
-        for (size_t j = 0; j < w; j++)
-        {
-            cout << matrix[i][j];
-        }
-        cout << endl;
-    }
+    // считаем 2 задачу (остальные столбцы проставляем в INVALID_VALUE)
+    sumIfNegative(matrix, h, w);
+    print(matrix, h + 1, w);
 
     //Освобождаем указатель каждой строки от ее столбцов
-    for(int i = 0; i < w; ++i) {
-        delete[] matrix[w];   
+    for (int i = 0; i <= h; ++i)
+    {
+        delete[] matrix[i];
     }
     // Освобождаем сами указатели строк
     delete[] matrix;
 
-    return 0; 
+    return 0;
 }
