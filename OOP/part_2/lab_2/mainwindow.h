55eb1b29fa598114b803c633c3b8779280886ce3
Максим Гречаниченко <ootmoons@gmail.com>
2024-01-07 01:17:38 +0300

    init lab2

---
+++ OOP/part_2/lab_2/mainwindow.h
@@ -0,0 +1,26 @@
+#include <QCheckBox>
+#include <QComboBox>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QVBoxLayout>
+#include <QtSql>
+#include <iostream>
+#include <map>
+
+class MainWindow : public QMainWindow {
+   public:
+    MainWindow() : QMainWindow() {
+        db.setDatabaseName("my_db.sqlite");
+        if (!db.open()) {
+            qDebug() << "Что-то пошло не так!";
+        }
+        
+    }
+
+   private:
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+};
