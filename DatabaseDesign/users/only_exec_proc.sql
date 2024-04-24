27cd07fa06bfbe35130f33439b6dea1ccc4df1af
getnly <gently.whitesnow@outlook.com>
2024-04-24 21:53:14 +0300

    comment

--- DatabaseDesign/users/only_exec_proc.sql
+++ DatabaseDesign/users/only_exec_proc.sql
@@ -1,12 +1,12 @@
 -- создаем юзера
 CREATE ROLE username WITH LOGIN PASSWORD 'password';
 
--- забираем все права
+-- забираем все права (дефолтные) кастомные не забираются
 REVOKE ALL ON DATABASE current_database() FROM username;
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM username;
 
--- выдаем права только на вызов процедур
+-- выдаем права только на вызов процедур (кастомное)
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO username;
 
--- даем права на подключение к бд
+-- даем права на подключение к бд (кастомное)
 GRANT CONNECT ON DATABASE current_database() TO username;
